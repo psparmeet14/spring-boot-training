aa112ad024cea0c927440c0901bd5a53133cc7b6
Parmeet Singh <psparmeet14@gmail.com>
2023-02-20 22:47:54 +0530

    feat: configured sqlserver and h2 datasource

--- src/main/java/com/parmeet/springboot/springboottraining/survey/model/Question.java
+++ src/main/java/com/parmeet/springboot/springboottraining/survey/model/Question.java
@@ -2,11 +2,13 @@ package com.parmeet.springboot.springboottraining.survey.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Question {
     private String id;
     private String name;
---
+++ src/test/java/com/parmeet/springboot/springboottraining/survey/resource/SurveyResourceTest.java
@@ -0,0 +1,7 @@
+package com.parmeet.springboot.springboottraining.survey.resource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SurveyResourceTest {
+
+}
