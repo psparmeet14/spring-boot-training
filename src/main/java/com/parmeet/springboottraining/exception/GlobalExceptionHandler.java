b40ef52a85e04e84e0c6490fb5aa179603a77f9d
Parmeet Singh <psparmeet14@gmail.com>
2023-05-07 20:17:46 +0530

    fix: minor fixes in property files

--- src/main/java/com/parmeet/springboottraining/exception/GlobalExceptionHandler.java
+++ src/main/java/com/parmeet/springboottraining/exception/GlobalExceptionHandler.java
@@ -1,46 +1,46 @@
-package com.parmeet.springboottraining.exception;
-
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import static java.util.Objects.nonNull;
-
-@Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
-@RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-    public static final String TRACE = "trace";
-
-    @Value("${dev.enable-stacktrace:false}")
-    private boolean printStackTrace;
-
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Object> handleAllUncaughtException(Exception exception, WebRequest request) {
-        log.error("Unknown error occurred", exception);
-        return buildErrorResponse(exception, "Unknown error occurred", HttpStatus.INTERNAL_SERVER_ERROR, request);
-    }
-
-    private ResponseEntity<Object> buildErrorResponse(Exception exception, String message, HttpStatus httpStatus, WebRequest request) {
-        var errorResponse = new ErrorResponse(httpStatus.value(), message);
-        if (isTraceOn(request)) {
-            errorResponse.setStackTrace(ExceptionUtils.getStackTrace(exception));
-        }
-        return ResponseEntity.status(httpStatus).body(errorResponse);
-    }
-
-    private boolean isTraceOn(WebRequest request) {
-        String[] value = request.getParameterValues(TRACE);
-        return nonNull(value)
-                && value.length > 0
-                && value[0].contentEquals("true");
-    }
-}
+//package com.parmeet.springboottraining.exception;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.commons.lang3.exception.ExceptionUtils;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//import org.springframework.web.context.request.WebRequest;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+//
+//import static java.util.Objects.nonNull;
+//
+//@Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
+//@RestControllerAdvice
+//public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+//
+//    public static final String TRACE = "trace";
+//
+//    @Value("${error.response.enable-stacktrace:false}")
+//    private boolean printStackTrace;
+//
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    public ResponseEntity<Object> handleAllUncaughtException(Exception exception, WebRequest request) {
+//        log.error("Unknown error occurred", exception);
+//        return buildErrorResponse(exception, "Unknown error occurred", HttpStatus.INTERNAL_SERVER_ERROR, request);
+//    }
+//
+//    private ResponseEntity<Object> buildErrorResponse(Exception exception, String message, HttpStatus httpStatus, WebRequest request) {
+//        var errorResponse = new ErrorResponse(httpStatus.value(), message);
+//        if (isTraceOn(request)) {
+//            errorResponse.setStackTrace(ExceptionUtils.getStackTrace(exception));
+//        }
+//        return ResponseEntity.status(httpStatus).body(errorResponse);
+//    }
+//
+//    private boolean isTraceOn(WebRequest request) {
+//        String[] value = request.getParameterValues(TRACE);
+//        return nonNull(value)
+//                && value.length > 0
+//                && value[0].contentEquals("true");
+//    }
+//}
